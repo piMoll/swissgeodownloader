1024585035de57f2dd7a9aea468de65a300c35a9
Pi Moll <pi1402@gmail.com>
2021-04-07 17:39:01 +0200

    Rename plugin root folder name.

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .swissgeodatadownloader import SwissGeodataDownloader
+    from .plugin.swissgeodatadownloader import SwissGeodataDownloader
     return SwissGeodataDownloader(iface)
