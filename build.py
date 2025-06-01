ac8a7c62fbd14d81d89a4c286b128b7b8157971c
Pi Moll <pimoll.dev@gmail.com>
2025-06-01 16:08:46 +0200

    Fix missing parent directory when creating finished plugin zip

--- build.py
+++ build.py
@@ -26,20 +26,22 @@ PKG_NAME = 'swissgeodownloader'
 PATTERN_EXCLUDES = ['__pycache__', '.pro', '.ts', 'scripts/', 'test/']
 
 
-def create_zip(zip_path, folder_path, ignore_patterns):
+def create_zip(zip_path, plugin_path, ignore_patterns):
     print('Creating ZIP archive ' + zip_path)
 
     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         
-        # # Readme is outside the plugin dir, so we start with it
+        # Readme is outside the plugin dir, so we start with it
         print('Adding README.md')
-        zipf.write(os.path.join(os.path.dirname(folder_path), 'README.md'),
-                   'README.md')
-        for root, folders, files in os.walk(folder_path):
+        zipf.write(os.path.join(plugin_path, os.pardir, 'README.md'),
+                   'swissgeodownloader/README.md')
+        # Go through all files in plugin dir and add them to zip archive
+        for root, folders, files in os.walk(plugin_path):
             for file in files:
                 path = str(os.path.join(root, file))
-                archive_path = str(
-                        os.path.relpath(os.path.join(root, file), folder_path))
+                archive_path = str(os.path.relpath(os.path.join(root, file),
+                                                   os.path.join(plugin_path,
+                                                                os.pardir)))
                 if not any(fnmatch(path, '*' + ignore + '*') for ignore in ignore_patterns):
                     print('Adding ' + archive_path)
                     zipf.write(path, archive_path)
